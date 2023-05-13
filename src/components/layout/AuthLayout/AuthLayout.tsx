6c65e25311d409c256e7f33706c283d112a923fd
ReisSouza <reis.souza@outlook.com.br>
2023-05-13 12:42:34 -0300

    feat: :sparkles: created auth layout

--- src/components/layout/AuthLayout/AuthLayout.tsx
+++ src/components/layout/AuthLayout/AuthLayout.tsx
@@ -1,18 +1,53 @@
 import React, { ReactNode } from 'react'
+import { IoLogIn } from 'react-icons/io5'
 
 import * as S from './styles'
+import { Button, Heading } from '@ionext-ui/react'
+import { Logo } from '@/components/Logo/Logo'
+import { useRouter } from 'next/router'
 
 export type AuthLayoutProps = {
   children: ReactNode
 }
 
 export const AuthLayout: React.FC<AuthLayoutProps> = ({ children }: AuthLayoutProps) => {
+  const { asPath, push } = useRouter()
+
+  const handleActionSecondaryPage = () => {
+    if (asPath.includes('login')) {
+      return {
+        label: 'Criar conta',
+        action: () => push('/auth/create-account'),
+      }
+    } else {
+      return {
+        label: 'Login',
+        action: () => push('/auth/login'),
+      }
+    }
+  }
   return (
     <S.AuthLayoutContainer>
-      <div className="wrappedLeft">
-        <S.WrappedLeft>{children}</S.WrappedLeft>
-      </div>
-      <div className="wrappedRight"></div>
+      <S.WrappedLeft>
+        <Logo />
+
+        <S.ContentLeft>{children}</S.ContentLeft>
+      </S.WrappedLeft>
+      <S.WrappedRight>
+        <Heading css={{ color: '$background', maxWidth: '580px' }} align="center">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam repudiandae aperiam vero accusantium, maiores
+          ipsum iusto obcaecati quos exercitationem expedita impedit sed commodi delectus. Voluptatum commodi facilis
+          facere. Iste, molestias?
+        </Heading>
+        <Button
+          onClick={handleActionSecondaryPage().action}
+          variant="contained"
+          color="tertiary"
+          iconRight={<IoLogIn size={24} />}
+        >
+          {handleActionSecondaryPage().label}
+        </Button>
+      </S.WrappedRight>
     </S.AuthLayoutContainer>
   )
 }
--- src/components/layout/AuthLayout/styles.ts
+++ src/components/layout/AuthLayout/styles.ts
@@ -1,37 +1,37 @@
 import { styled } from '@ionext-ui/react'
 
 export const AuthLayoutContainer = styled('div', {
-  display: 'flex',
-  flexDirection: 'row',
+  display: 'grid',
   width: '100%',
   height: '100vh',
-  '& > *': {
-    boxSizing: 'border-box',
-  },
-  '& > .wrappedLeft': {
-    flex: '0 0 55%',
-    height: '100%',
+  gridTemplateColumns: '55vw 45vw',
+  overflowY: 'auto',
+  overflowX: 'hidden',
+})
 
-    paddingTop: 'calc($20 * 2)',
-  },
-  '& > .wrappedRight': {
-    flex: '0 0 45%',
-    height: '100%',
-    background: '$primary-normal',
-  },
-  '@media (max-width: 1000px)': {
-    flexDirection: 'column',
-    '& > *': {
-      flex: '1 1 auto',
-    },
-  },
+export const WrappedRight = styled('div', {
+  minWidth: '45%',
+  background: 'linear-gradient(151.62deg, #EC509A 0%, rgba(83, 28, 54, 0.63) 46.88%, #4A5899 98.66%)',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  gap: '$6',
 })
 
 export const WrappedLeft = styled('div', {
   width: '100%',
-  maxWidth: '480px',
-  margin: 'auto',
-  height: '100%',
+  padding: '$10 0',
+  flex: 1,
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '$8',
+  justifyContent: 'center',
+})
+
+export const ContentLeft = styled('div', {
   display: 'flex',
   alignItems: 'center',
+  margin: '0 auto',
+  width: '600px',
 })
